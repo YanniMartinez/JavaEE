3c1ea93474cb59f338fc2430ce3ded91c0939f32
Yann <metalyanni@gmail.com>
2022-08-13 00:05:09 -0500

    Creación de Modelo, interface y servicio dedicada a un PRODUCTO

---
+++ 42-Cabeceras-http/webapp-headers/src/main/java/org/ymartinezm/apiservlet/webapp/headers/services/ProductoServiceImpl.java
@@ -0,0 +1,18 @@
+package org.ymartinezm.apiservlet.webapp.headers.services;
+
+import org.ymartinezm.apiservlet.webapp.headers.models.Producto;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductoServiceImpl implements ProductoService {
+
+    @Override
+    public List<Producto> listar() {
+        //Retornando un arreglo de 3 elementos tipo Producto
+        return Arrays.asList( new Producto( 1L, "Notebook", "Computación",175000),
+                new Producto(2L, "Mesa Escritorio", "Oficina",10000),);
+                new Producto(3L, "Teclado Mecánico", "Computación",40000));
+
+    }
+}
