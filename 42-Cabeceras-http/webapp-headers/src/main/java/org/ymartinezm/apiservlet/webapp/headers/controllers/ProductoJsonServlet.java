b44095be83f2a104bb20150d8a1834c56b94c553
Yann <metalyanni@gmail.com>
2022-08-13 15:35:11 -0500

    Recibiendo JSON en el BODY del REQUEST

--- 42-Cabeceras-http/webapp-headers/src/main/java/org/ymartinezm/apiservlet/webapp/headers/controllers/ProductoJsonServlet.java
+++ 42-Cabeceras-http/webapp-headers/src/main/java/org/ymartinezm/apiservlet/webapp/headers/controllers/ProductoJsonServlet.java
@@ -2,6 +2,7 @@ package org.ymartinezm.apiservlet.webapp.headers.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,6 +12,7 @@ import org.ymartinezm.apiservlet.webapp.headers.services.ProductoService;
 import org.ymartinezm.apiservlet.webapp.headers.services.ProductoServiceImpl;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 @WebServlet("/productos.json")
@@ -26,6 +28,39 @@ public class ProductoJsonServlet extends HttpServlet {
         //Con esto ahora nuestro Servlet puede trabajar como API REST
         resp.setContentType("application/json");
         resp.getWriter().write(json);
+
+    }
+
+    /**
+     * Método para recibir un JSON dentro del body del Request
+     * */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletInputStream jsonStream = req.getInputStream(); //Recibiendo del InputStream
+        ObjectMapper mapper = new ObjectMapper();
+        //Convirtiendo JSON inputStream a un objeto (Producto.class)
+        Producto producto = mapper.readValue(jsonStream, Producto.class); //Con esto ya tenemos el objeto y podemos usarlo
+
+        resp.setContentType("text/html; charset=UTF-8");
+        try (PrintWriter out = resp.getWriter()) {
+
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("    <head>");
+            out.println("        <meta charset=\"UFT-8\">");
+            out.println("        <title>Detalle de producto desde JSON</title>");
+            out.println("    </head>");
+            out.println("    <body>");
+            out.println("        <h1>Detalle de producto desde JSON</h1>");
+            out.println("        <ul>");
+            out.println("           <li>Id: "+ producto.getId() +"</li>");
+            out.println("           <li>Nombre: "+ producto.getNombre() +"</li>");
+            out.println("           <li>Tipo: "+ producto.getTipo() +"</li>");
+            out.println("           <li>Precio: "+ producto.getPrecio() +"</li>");
+            out.println("        </ul>");
+            out.println("    </body>");
+            out.println("</html>");
+        }
 
     }
 }
