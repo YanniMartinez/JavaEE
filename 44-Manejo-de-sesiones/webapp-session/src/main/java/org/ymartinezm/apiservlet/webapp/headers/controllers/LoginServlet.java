21eaad6d42e2f566797a926d66f59db822bd7c96
Yann <metalyanni@gmail.com>
2022-08-29 17:36:56 -0500

    Proyecto base, manejo de sesiones con HTTP Session

---
+++ 44-Manejo-de-sesiones/webapp-session/src/main/java/org/ymartinezm/apiservlet/webapp/headers/controllers/LoginServlet.java
@@ -0,0 +1,74 @@
+package org.ymartinezm.apiservlet.webapp.headers.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.ymartinezm.apiservlet.webapp.headers.services.LoginService;
+import org.ymartinezm.apiservlet.webapp.headers.services.LoginServiceImp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Optional;
+
+@WebServlet({"/login","/login.html"})
+public class LoginServlet extends HttpServlet {
+
+    final static String USERNAME="admin";
+    final static String PASSWORD="12345";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LoginService auth = new LoginServiceImp();
+        Optional<String> cookieOptional = auth.getUsername(req);
+
+        if(cookieOptional.isPresent()){ //Si existe coookie no presenta login, sino contenido custom
+
+            resp.setContentType("text/html; charset=UTF-8");
+            try (PrintWriter out = resp.getWriter()) {
+
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("    <head>");
+                out.println("        <meta charset=\"UFT-8\">");
+                out.println("        <title>Hola" + cookieOptional.get()+"</title>");
+                out.println("    </head>");
+                out.println("    <body>");
+                out.println("        <h1>Hola " + cookieOptional.get()+" has iniciado sesión anteriormente</h1>");
+                out.println("<p><a href='" + req.getContextPath() +"/index.html'>Volver  </a>  </div>");
+                out.println("<p><a href='" + req.getContextPath() +"/logout'>Cerrar session  </a>  </div>");
+                out.println("    </body>");
+                out.println("</html>");
+            }
+
+        }else { //No hay cookie PRESENTA login
+            //Estableciendo el Dispatcher para propagar info entre la JSP y el servlet
+            getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp); //Carga JSP
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if ( USERNAME.equals(username) && PASSWORD.equals(password)) {
+
+            //Forma de declarar una nueva cookie (Formato clave:Valor)
+            Cookie usernameCookie = new Cookie("username",username);
+
+            //Manando cookie en el response para que sea interpretada por el navegador
+            resp.addCookie(usernameCookie);
+
+            resp.sendRedirect(req.getContextPath()+"/login.html");
+
+        }else{
+            //resp.setStatus(401);
+            //Otra alternativa es:
+            resp.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Lo sentimos, no está autorizado para ingresar a esta página");
+        }
+    }
+}
---
+++ 44-Manejo-de-sesiones/webapp-session/src/main/java/org/ymartinezm/apiservlet/webapp/headers/controllers/ProductoServlet.java
@@ -0,0 +1,80 @@
+package org.ymartinezm.apiservlet.webapp.headers.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.ymartinezm.apiservlet.webapp.headers.models.Producto;
+import org.ymartinezm.apiservlet.webapp.headers.services.LoginService;
+import org.ymartinezm.apiservlet.webapp.headers.services.LoginServiceImp;
+import org.ymartinezm.apiservlet.webapp.headers.services.ProductoService;
+import org.ymartinezm.apiservlet.webapp.headers.services.ProductoServiceImpl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@WebServlet({"/productos.html", "/productos"})
+public class ProductoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        //Creación de servicio basado en la implementación
+        ProductoService service = new ProductoServiceImpl();
+        List<Producto> productos = service.listar(); //Obtenemos los productos
+
+        LoginService auth = new LoginServiceImp();
+        Optional<String> cookieOptional = auth.getUsername(req);
+
+
+        resp.setContentType("text/html; charset=UTF-8");
+
+        try (PrintWriter out = resp.getWriter()) {
+
+
+            out.print("<!DOCTYPE html>");
+            out.print("<html>");
+            out.print("    <head>");
+            out.print("        <meta charset=\"UFT-8\">");
+            out.print("        <title>Listado de Productos</title>");
+            out.print("    </head>");
+            out.print("    <body>");
+            out.print("        <h1>Listado de Productos</h1>");
+
+            if(cookieOptional.isPresent()){
+                out.println("<div style='color:blue;'> Hola " + cookieOptional.get() +"</div>");
+            }
+
+            out.print("        <table>");
+            out.print("             <tr>");
+            out.print("                 <th>Id</th>");
+            out.print("                 <th>Nombre</th>");
+            if(cookieOptional.isPresent()) {
+                out.print("                 <th>Tipo</th>");
+            }
+            out.print("                 <th>Precio</th>");
+            out.print("             </tr>");
+
+            //Iterando la lista de productos
+            productos.forEach(p -> {
+                out.print("             <tr>");
+                out.print("             <td>" + p.getId() + "</td>");
+                out.print("             <td>" + p.getNombre() + "</td>");
+                out.print("             <td>" + p.getTipo() + "</td>");
+                if(cookieOptional.isPresent()) {
+                    out.print("             <td>" + p.getPrecio() + "</td>");
+                }
+                out.print("             </tr>");
+            });
+
+            out.print("        </table>");
+
+        }
+
+    }
+}
